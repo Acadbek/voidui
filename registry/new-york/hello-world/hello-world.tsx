debd1d08148a21461a92b2f3a1dc27f609e30318
Asad <ibnnumon@gmail.com>
2025-09-01 05:48:26 +0500

    update

---
+++ registry/new-york/hello-world/hello-world.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export function HelloWorld() {
+  return <Button className="text-2xl font-bold">Hello World</Button>
+}
--- src/utils/props.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export type HTMLButtonProps = React.HTMLProps<HTMLButtonElement>;
-
-export interface IProps {
-  style?: React.CSSProperties;
-  className?: string;
-  prefixCls?: string;
-}
