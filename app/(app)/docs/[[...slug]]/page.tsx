0c1bbbce0c0df01a7c0a9f698117a8077ae2fe16
Asad <ibnnumon@gmail.com>
2025-10-02 04:33:41 +0500

    config headers

--- app/(app)/docs/[[...slug]]/page.tsx
+++ app/(app)/docs/[[...slug]]/page.tsx
@@ -1,8 +1,4 @@
 import * as React from "react"
-import { Metadata } from "next"
-import { notFound } from "next/navigation"
-
-import { siteConfig } from "@/config/site"
 
 export default function page() {
   return (
